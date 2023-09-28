72524d39f2b0671b57f6d2dd8f253520b9e652f1
Ykrop4lk <Tropic192837465@gmail.com>
2023-09-28 21:14:06 +0300

    Full rewriting Initial commit

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
 import configparser
 import subprocess
-import sqlite3
-
-conn = sqlite3.connect("database/tasks.db")
-cur = conn.cursor()
-
-cur.execute('''
-        CREATE TABLE IF NOT EXISTS tasks (
-            id INTEGER PRIMARY KEY,
-            task_id TEXT,
-            title TEXT,
-            price TEXT,
-            "date" TEXT,
-            responce TEXT,
-            description TEXT,
-            tags TEXT,
-            href TEXT
-        )
-    ''')
-conn.commit()
-conn.close()
 
 print("Starting...")
 
@@ -27,8 +7,6 @@ settings = configparser.ConfigParser()
 
 if not settings.read('Keys.ini'):
     telegram_key = input('Для начала работы введите Telegram токен:')
-    habr_link = input('Для начала работы введите Ссылку на Habr с параметрами поиска:')
-    settings['PARAMS'] = {'telegram_key': telegram_key, 'habr_link': habr_link}
     with open('Keys.ini', 'w') as config_file:
         settings.write(config_file)
         config_file.close()
--- telegram.py
+++ telegram.py
@@ -16,81 +16,14 @@ keys = configparser.ConfigParser()
 keys.read('Keys.ini')
 
 bot = Bot(token = keys.get('PARAMS', 'telegram_key'))
-habr_url = keys.get('PARAMS', 'habr_link')
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
-# @dp.message_handler()
-# async def id(message: types.Message):
-#     print(message.forward_from_chat.id)
-
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Привет! Я начну опрашивать БД и отправлять обновления в канал.")
-    last_upd = await check_db_changes() # Последний обработанный id
-    while True:
-        print(1)
-        Habr_Scrapper.get_parsed_info(habr_url)
-        db_changes = await check_db_changes()
-        if last_upd != db_changes:
-            last_upd_last = last_upd
-            last_upd = db_changes
-            number_list = list(range(last_upd_last + 1, last_upd + 1))
-            asyncio.create_task(send_task(number_list, message.chat.id))
 
-        await asyncio.sleep(15)
-
-
-async def send_task(list, chat_id):
-    for tasks in list:
-        task_id = tasks_db_operator.get_task_id(tasks)
-        keyboard = InlineKeyboardMarkup(row_width=1)
-        keyboard.add(InlineKeyboardButton(text="Ссылка", url=tasks_db_operator.get_url_by_task_id(task_id)))
-        keyboard.add(InlineKeyboardButton(text="Показать полное описание", callback_data=f"full_description:{task_id}"))
-
-        text = (f"*{tasks_db_operator.get_title_by_task_id(task_id)}*"
-                f"\n\n*{tasks_db_operator.get_price_by_task_id(task_id)}*"
-                f"\n\n_{tasks_db_operator.get_desc_by_task_id(task_id)[:200]}_"
-                f"\n\n_{tasks_db_operator.get_responce_by_task_id(task_id)} Откликов_"
-                f"\n\n`{tasks_db_operator.get_tags_by_task_id(task_id)}`"
-                f"\n`{tasks_db_operator.get_date_by_task_id(task_id)}`")
-
-        await bot.send_message(text=text, chat_id="-1001958772967", reply_markup=keyboard, parse_mode="markdown")
-        await asyncio.sleep(5)
-
-@dp.callback_query_handler(lambda c: c.data == "delete")
-async def process_callback(callback_query: types.CallbackQuery):
-    await bot.delete_message(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id)
-
-@dp.callback_query_handler(lambda c: "full_description" in c.data)
-async def process_callback(callback_query: types.CallbackQuery):
-    task_id = callback_query.data.split(":")[1]
-    keyboard = InlineKeyboardMarkup(row_width=1)
-    keyboard.add(InlineKeyboardButton(text="Ссылка", url=tasks_db_operator.get_url_by_task_id(task_id)))
-    keyboard.add(InlineKeyboardButton(text="Закрыть", callback_data="delete"))
-
-    text = (f"*{tasks_db_operator.get_title_by_task_id(task_id)}*"
-            f"\n\n*{tasks_db_operator.get_price_by_task_id(task_id)}*"
-            f"\n\n_{tasks_db_operator.get_desc_by_task_id(task_id)}_"
-            f"\n\n_{tasks_db_operator.get_responce_by_task_id(task_id)} Откликов_"
-            f"\n\n`{tasks_db_operator.get_tags_by_task_id(task_id)}`"
-            f"\n`{tasks_db_operator.get_date_by_task_id(task_id)}`")
-
-    await bot.send_message(text=text, chat_id="-1001958772967", reply_markup=keyboard, parse_mode="markdown")
-
-
-async def check_db_changes():
-    conn = sqlite3.connect('database/tasks.db')  # Замените на имя вашей БД
-    cursor = conn.cursor()
-    last_record = 0
-    try:
-        cursor.execute("SELECT * FROM tasks ORDER BY id DESC LIMIT 1")  # Замените на вашу таблицу и поле
-        last_record = cursor.fetchone()[0]
-    except:
-        pass
-
-    return last_record
 
 
 if __name__ == '__main__':
